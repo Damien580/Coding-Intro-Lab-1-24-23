a316e9c8fc414f1220bd3cd5362e17171cc3bfc0
Damien Redd <damiens.redd@gmail.com>
2023-01-24 12:40:40 -0600

    Completed maxOfTwo.js coding challenge.

---
+++ maxOfTwo.js
@@ -0,0 +1,10 @@
+let x = 1;
+let y = 2;
+
+if(x > y) {
+   console.log(`The largest value is ${x}`)
+} else if (y > x) {
+    console.log(`Thelargest value is ${y}`)
+} else {
+    console.log(`The values, ${y} and ${x}, are identical.`)
+}
